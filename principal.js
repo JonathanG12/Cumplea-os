1ae25c8a6b178f8be181008f85908b840c4d7e87
JonathanG12 <65064232+JonathanG12@users.noreply.github.com>
2024-06-12 17:08:15 -0600

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,19 @@
+const countdownDate = new Date("2024-06-18T05:30:00-00:31");
+const countdownElement = document.querySelector(".countdown");
+
+function updateCountdown() {
+    const now = new Date();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
